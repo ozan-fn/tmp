125ba2a974bbb70042a8985c319bb28e72bf4c47
ozan-fn <me@ozan.my.id>
2026-03-10 09:28:35 +0700

    feat: replace Google AdSense meta tag with script for ad integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {};
 export default function Home() {
     return (
         <>
-            <meta name="google-adsense-account" content="ca-pub-1777096429478325" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1777096429478325" crossOrigin="anonymous"></script>
 
             <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
                 <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
